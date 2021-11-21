708473ef43bccb30d256322a2b9a6ea697cee83c
zefirlover <ibodikmushta@gmail.com>
2021-11-21 16:15:16 +0200

    Controller added

---
+++ RozkladSharpReworked/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RozkladSharp.DomainServices;
+
+namespace RozkladSharpReworked.Controllers
+{
+    public class HomeController : Controller
+    {
+        RozkladSharpDbContext _context;
+        public HomeController(RozkladSharpDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Students.ToList());
+        }
+    }
+}
